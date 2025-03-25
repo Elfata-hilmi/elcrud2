2cdbcbec7e3dfa3639da3d26c814fc5f8ea9fe78
ZnKovalskia <onigen.demon@gmail.com>
2025-03-25 13:38:22 +0700

    Update Razaan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,9 +46,6 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Pegawai</h1>
-                    </div>
                     @yield('content')
                     <!-- Content Row -->
                     
